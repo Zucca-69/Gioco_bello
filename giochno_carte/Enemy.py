459219938429db654eed854f2d423c95929d4e2c
Damian-theDev <monicabenini.2006@gmail.com>
2024-02-16 15:47:32 +0100

    modified:   giochno_carte/Enemy.py

--- giochno_carte/Enemy.py
+++ giochno_carte/Enemy.py
@@ -1,17 +1,17 @@
 class Enemy:
     def __init__(self,card):
         self.__healt=0
-        self.__attcack=0
+        self.__attack=0
         self.__card=card
 
     def stats(self):
         if self.__card[0][0]=="K":
             self.__healt=40
-            self.__attcack=20
+            self.__attack=20
         elif self.__card[0][0]=="Q":
             self.__healt=30
-            self.__attcack=15
+            self.__attack=15
         elif self.__card[0][0]=="J":
             self.__healt=20
-            self.__attcack=10
-        return self.__healt, self.__attcack
+            self.__attack=10
+        return self.__healt, self.__attack
