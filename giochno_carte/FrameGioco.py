4527071e9d061e044869a1031186403fbe01e3bd
Damian-theDev <monicabenini.2006@gmail.com>
2024-04-28 07:57:19 +0200

    il gioco vede le carte (forse c'è una speranza)

--- giochno_carte/FrameGioco.py
+++ giochno_carte/FrameGioco.py
@@ -1,7 +1,6 @@
 import tkinter as tk
 from PIL import ImageTk, Image
 import os
-from game_logic import Game
 
 current_dir = os.path.dirname(__file__)
 image_path = os.path.join(current_dir, "imm", "OIUGSZ0.jpg")
@@ -13,29 +12,47 @@ except Exception as e:
     print("Errore durante il caricamento dell'immagine:", e)
 
 class GameUI:
-    def __init__(self, master, game_instance):
-        self.master = master
-        self.__game = game_instance
+    def __init__(self, enemy_card):
+        self.master = tk.Tk()
+        self.__game = None
         self.master.title("Regicide - partita in corso")
         self.master.geometry("1000x600")
         self.master.configure(bg="#106040")
         self.master.minsize(1100, 600)
-        
+
+        self.__enemy_card = enemy_card
+
+    def update_enemy_card(self, card):
+        self.__enemy_card = card
+        self.__show_card(self.__frame_castello, self.__enemy_card)
+
+    def add_game_instance(self, instance):
+        self.__game = instance
+        self.__init_widgets()
+
+    def __init_widgets(self):
+        # imposto il frame del castello (e mostro il nemico)
+        self.__frame_castello = tk.Frame(self.master, width=142, height=200, bg="#106040")
+        self.__frame_castello.place(anchor='center', relx=0.5, rely=0.35)
+        self.__show_card(self.__frame_castello, self.__enemy_card)
+
+        # imposto frame scarti
+        self.__frame_scarti = tk.Frame(self.master, width=142, height=200, bg="#106040")
+        self.__frame_scarti.place(anchor='center', relx=0.3, rely=0.35)
+        self.__scarto = None
+
+        # imposto frame del giocatore 
         self.__player_frame = tk.Frame(self.master)
         self.__player_frame.pack(side=tk.BOTTOM, pady=20)
 
-        self.__init_widgets()
-
-    def __init_widgets(self):        
-        self.__frame_castello = tk.Frame(self.master, width=450, height=300, bg="#106040")
-        self.__frame_castello.pack()
-        self.__frame_castello.place(anchor='center', relx=0.5, rely=0.35)
-        self.__show_card(self.__frame_castello, self.__game.get_enemy_card())
-
+        # mostro le carte del giocatore
         self.__show_player_cards()
 
+        # metto il tasto per la rinuncia 
         self.__button_rinuncia = tk.Button(self.master, text="Rinuncia", command=self.__rinuncia_turno)
         self.__button_rinuncia.place(relx=0.97, rely=0.97, anchor='se')
+
+        self.master.mainloop()
 
     def __show_player_cards(self):
         for card in self.__game.get_player_cards():
@@ -44,39 +61,44 @@ class GameUI:
             self.__show_card(frame, card)
 
     def __card_clicked(self, card):
-        self.__game.process_card(card)
+        self.__game.got_card(card)
+        
+        self.__scarto = card
+        self.__show_card(self.__frame_scarti, card)
 
-    def __update_player_showed_cards(self, new_cards):
+    def update_player_showed_cards(self, new_cards):
         for widget in self.__player_frame.winfo_children():
             widget.destroy()
 
         self.__show_player_cards()
 
     def __update_mano_player(self, mano):
-        self.__update_player_showed_cards(mano)
+        self.update_player_showed_cards(mano)
 
     def __show_card(self, frame, card):
-        card_coordinates = coordinate_carte[card.split("_")[1]][card]
+        if card != None:
+            card_coordinates = coordinate_carte[card.split("_")[1]][card]
 
-        left, top = card_coordinates[0], card_coordinates[1]
-        crop_region = (left, top, left + 410, top + 570)
+            left, top = card_coordinates[0], card_coordinates[1]
+            crop_region = (left, top, left + 410, top + 570)
 
-        try:
-            cropped_img = original_img.crop(crop_region)
-            cropped_img = cropped_img.resize((142, 200), Image.BICUBIC)
+            try:
+                cropped_img = original_img.crop(crop_region)
+                cropped_img = cropped_img.resize((142, 200), Image.BICUBIC)
 
-            img = ImageTk.PhotoImage(cropped_img)
+                img = ImageTk.PhotoImage(cropped_img)
 
-            button = tk.Button(frame, image=img, bd=0, command=lambda: self.__card_clicked(card))
-            button.image = img
-            button.pack()
+                button = tk.Button(frame, image=img, bd=0, command=lambda: self.__card_clicked(card))
+                button.image = img
+                button.pack()
 
-        except Exception as e:
-            print("Errore durante la visualizzazione dell'immagine:", e)    
+            except Exception as e:
+                print("Errore durante la visualizzazione dell'immagine:", e)    
 
     def __rinuncia_turno(self):
         self.__game.rinuncia_turno()
 
+global coordinate_carte
 coordinate_carte = {
     'quadri': {'K_quadri': (90, 545), 'Q_quadri': (90, 1205), 'J_quadri': (90, 1865), '10_quadri': (90, 2525), '9_quadri': (90, 3185), '8_quadri': (90, 3845), '7_quadri': (2520, 545), '6_quadri': (2520, 1205), '5_quadri': (2520, 1865), '4_quadri': (2520, 2525), '3_quadri': (2520, 3185), '2_quadri': (2520, 3845), 'A_quadri': (2043, 2525)}, 
     'picche': {'K_picche': (582, 545), 'Q_picche': (582, 1205), 'J_picche': (582, 1865), '10_picche': (582, 2525), '9_picche': (582, 3185), '8_picche': (582, 3845), '7_picche': (3012, 545), '6_picche': (3012, 1205), '5_picche': (3012, 1865), '4_picche': (3012, 2525), '3_picche': (3012, 3185), '2_picche': (3012, 3845), 'A_picche': (2043, 1865)}, 
--- giochno_carte/GameCheated.py
+++ giochno_carte/GameCheated.py
@@ -2,6 +2,8 @@ from Mazzo import *
 from Player import *
 from Enemy import *
 
+from FrameGioco import *
+
 class Game:
     def __init__(self, n_giocatori):
         self.__n_giocatori = n_giocatori
@@ -12,9 +14,15 @@ class Game:
 
         self.__setup_game()
 
-    def __setup_game(self):
-        # Inizializza il gioco, crea i giocatori, distribuisce le carte, etc.
-        pass
+        self.__gui = GameUI(self.__nemico.getEnemyCard()[0])
+        self.__gui.add_game_instance(self)
+
+    def cambio_turno(self):
+        if self.__current_player_index + 1 == self.__n_giocatori:
+            self.__current_player_index = 0
+        self.__animale = True
+        self.__lista_giocate = []
+        self.__tot = 0
 
     def get_player_cards(self):
         return self.__player_cards
@@ -22,10 +30,78 @@ class Game:
     def get_enemy_card(self):
         return self.__enemy_card
 
-    def process_card(self, card):
-        # Elabora la carta selezionata dal giocatore
-        pass
+    def got_card(self, card):
+        if card[0]=="A" and self.__animale: # se la prima carta è un animale
+            self.__gui.update_player_showed_cards()
 
     def rinuncia_turno(self):
-        # Elabora la rinuncia al turno
-        pass
+        # se rinuncio il turno
+        if len(self.__lista_giocate) == 0: # e non ho giocato carte
+            self.cambio_turno() # tocca al prossimo
+        else: 
+            attacca_nemico()
+
+    def attacca_nemico(self):
+        pass 
+
+    def __setup_game(self):
+        #creo gli scarti
+        self.__scarti=Mazzo()
+
+        #creo il mazzo nemico
+        self.__castello=Mazzo()
+        for seme in ["picche","fiori","quadri","cuori"]:
+            self.__castello.addCard("K", seme)
+            self.__castello.addCard("J", seme)
+            self.__castello.addCard("Q", seme)
+        self.__castello.shuffle()
+
+        # pesco nemico 
+        self.__nemico= Enemy()
+        self.__nemico.addStats(self.__castello.pickCard())
+
+        #creo il mazzo da cui pescare
+        self.__taverna=Mazzo()
+        for seme in ["picche","cuori","fiori","quadri"]:
+            self.__taverna.addCard("A", seme)
+            for numero in range(2,11):
+                self.__taverna.addCard(str(numero), seme)
+
+        #condizioni di gioco
+        self.__re= 4
+        self.__continua=True
+        
+    
+    def creaGiocatori(self):
+        # istanzio i giocatori e creo la lista giocatori 
+        player_0 = Player() 
+        player_1 = Player()
+        player_2 = Player()
+        player_3 = Player()
+
+        giocatori = [player_0, player_1, player_2, player_3]
+        self.__giocatori = giocatori[:self.__n_giocatori]
+        
+        # calcolo num max delle carte in mano e aggiungo eventuali jolly 
+        numGiocatori = len(self.__giocatori)
+        self.__numMaxCarte = 8
+        i = 0
+        while numGiocatori > 1:
+            if numGiocatori > 2: 
+                self.__taverna.addCard(f"Jolly{i}")
+                i += 1
+            numGiocatori -= 1
+            self.__numMaxCarte -= 1
+
+        # mescolo il mazzo
+        self.__taverna.shuffle()
+        
+        # imposto val num max carte e faccio pescare i vari giocatori
+        for giocatore in giocatori:
+            giocatore.setMaxCarte(self.__numMaxCarte)
+
+        for _ in range(self.__numMaxCarte):
+            for i in giocatori:
+                i.draw(self.__taverna.pickCard())
+
+giocata = Game(2)
